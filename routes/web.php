399d1c1be39eca10102924d001ca9a4c467e424a
ZaizafunHanifah <hanifah9158@gmail.com>
2025-05-30 11:12:44 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TransaksiController;
+use App\Http\Controllers\TabunganController;
 
 // Redirect root ke halaman login
 Route::get('/', function () {
@@ -26,3 +27,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::post('/transaksi', [TransaksiController::class, 'store'])->name('transaksi.store');
 });
+
+Route::get('/transaksi', [TransaksiController::class, 'create'])->name('transaksi.create');
+Route::post('/transaksi', [TransaksiController::class, 'store'])->name('transaksi.store');
+
+Route::get('/tabungan', [TabunganController::class, 'index'])->name('tabungan.index');
+Route::get('/tabungan', [TransaksiController::class, 'tabunganIndex'])->name('tabungan.index');
